0f33575f21c0e5d8c0800b7f7d882b3436c0f1ab
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-27 09:55:28 +0000

    [Add]topページとaboutページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images
+    @books = @user.books
   end
 
   def edit
   end
+  
+  def index
+     @users = User.all
+  end
+  
+  def update
+  end
+  
 end
