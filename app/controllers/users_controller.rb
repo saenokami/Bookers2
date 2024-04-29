993bd7f84f20ecbfc4b8d3e3aaf9041a9e5b1fed
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-29 00:48:18 +0000

    [Add]user/showページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books if @user.present?
   end
 
   def edit
