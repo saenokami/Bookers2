5bc28a3237c0db6190199f05df52a93d454da8ca
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-29 08:37:00 +0000

    [Update]aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
  
- get 'homes/about', to: 'homes#about', as: 'about'
- root to: 'homes#top'
+ get '/home/about', to: 'homes#about', as: 'about'
+
+  root to: 'homes#top'
   devise_for :users
   
   resources :books, only: [:create, :new, :index, :show, :edit,:update, :destroy]
