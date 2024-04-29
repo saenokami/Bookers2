e7a69dec90fdbe28c96259bebd78ffc840f6ad61
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-29 14:05:22 +0000

    [Update]他人の情報をサイドに表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_book, only: [:edit, :update, :destroy]
-  
 
-  
+
+
   def create
     @book = Book.new(book_params)
     @book.user = current_user # 現在のユーザーを書籍の所有者として設定する
@@ -12,22 +12,24 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: 'You have created book successfully.' # 保存に成功した場合のリダイレクト先とメッセージ
     else
       @books = Book.all
-      @user_info = current_user 
+      @user_info = current_user
       render :index
     end
   end
-  
+
 
   def index
-   
+
     @books = Book.all
     @user_info = current_user # ログインしているユーザーの情報
     @book = Book.new # 新しい書籍を作成するための空のBookインスタンス
   end
 
   def show
+    @user = current_user
     @book = Book.find(params[:id])
     @new_book = Book.new
+
   end
 
   def edit
@@ -37,7 +39,7 @@ class BooksController < ApplicationController
         redirect_to books_path
       end
   end
-  
+
   def update
      @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -46,21 +48,21 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @book = Book.find(params[:id]) # 削除するPostImageレコードを取得
     @book.destroy # レコードを削除
     redirect_to books_path # PostImageの一覧ページへのパス
-  end 
-  
-  
+  end
+
+
   # ストロングパラメータ
   private
 
   def book_params
     params.require(:book).permit(:title, :body) # 許可するパラメーターを指定する
   end
-  
+
   def set_book
     @book = Book.find(params[:id])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
-  
+
+
   def show
+    @user = current_user
     @user = User.find(params[:id])
-    @books = @user.books if @user.present?
+    @books = @user.books
     @book = Book.new
+
   end
 
   def edit
@@ -15,26 +17,27 @@ class UsersController < ApplicationController
        redirect_to user_path(current_user.id)
      end
   end
-  
+
   def index
+     @user = current_user
      @users = User.all
      @new_book = Book.new
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: 'You have updated user successfully.'
     else
-     
+
       render :edit
     end
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
